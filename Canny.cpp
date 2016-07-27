62c406671dde54c896ca76549b90fda3b31e5e49
Kevin898y <kevin888y@gmail.com>
2016-07-28 04:32:24 +0800

    Canny Edge Detector

---
+++ Canny.cpp
@@ -0,0 +1,22 @@
+﻿#include <cv.h>
+#include <highgui.h>
+using namespace std;
+using namespace cv;
+int main()
+{
+	
+	Mat test;
+	//載入灰階
+	test = imread("D:\\1.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+	cvNamedWindow("Gray", 1);
+	imshow("Gray", test);
+	GaussianBlur(test, test, Size(3, 3), 0, 0);//3x3
+	Mat Edge;
+	Canny(test, Edge, 50,150);//Min Threshold 50
+	cvNamedWindow("CannyEdge", 1);
+	imshow("CannyEdge", Edge);
+
+	cvWaitKey(0);
+	cvDestroyWindow("Gray");
+	cvDestroyWindow("CannyEdge");
+}
