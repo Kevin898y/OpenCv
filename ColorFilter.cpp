4ad96afd178ec87fcf14b677df1f556afa0a50cc
Kevin898y <kevin888y@gmail.com>
2016-08-04 14:37:34 +0800

    color Filter

---
+++ ColorFilter.cpp
@@ -0,0 +1,18 @@
+﻿#include <cv.h>
+#include <highgui.h>
+using namespace std;
+using namespace cv;
+int main()
+{
+	Mat Orange, hsv;
+
+	Orange = imread("1.jpg");
+	cvtColor(Orange, hsv, CV_BGR2HSV);
+	//R=Scalar(0, 100, 100), Scalar(10, 255, 255),B=Scalar(110, 50, 50), Scalar(130, 255, 255),
+	inRange(hsv, Scalar(50, 100, 100), Scalar(70, 255, 255), hsv);//R
+
+	imshow("Orange", Orange);
+	imshow("Filter", hsv);
+	waitKey(0);
+
+}
