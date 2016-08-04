ba4dfd490f4fdf8edab232a2baae6a16a0adbf38
Kevin898y <kevin888y@gmail.com>
2016-08-04 14:38:33 +0800

    Erode and dilate

---
+++ Erode.cpp
@@ -0,0 +1,19 @@
+﻿#include <cv.h>
+#include <highgui.h>
+using namespace std;
+using namespace cv;
+int main()
+{
+	Mat Orange, Thresh, Erode, Dilate;
+
+	Orange = imread("D:/1.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+	threshold(Orange, Thresh, 127, 255, THRESH_BINARY);
+	erode(Thresh, Erode, Mat(), Point(-1, -1), 1);
+	dilate(Thresh, Dilate, Mat(), Point(-1, -1), 1);
+
+	imshow("threshold", Thresh);
+	imshow("erode", Erode);
+	imshow("dilate", Dilate);
+	waitKey(0);
+
+}
